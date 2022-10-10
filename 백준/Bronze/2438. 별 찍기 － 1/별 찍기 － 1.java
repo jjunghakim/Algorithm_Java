8e9c192ce90437aa20b2b42861cfa7a9ed0d71b1
jjunghakim <111368350+jjunghakim@users.noreply.github.com>
2022-10-11 00:11:29 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 212 ms, Memory: 14876 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+
+        int N = Integer.parseInt(st.nextToken());
+
+        for(int i = 1; i <= N; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
