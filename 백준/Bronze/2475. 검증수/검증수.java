a72d2ec48650a29af028c456e87fe2bcb6836bf1
jjunghakim <111368350+jjunghakim@users.noreply.github.com>
2022-10-11 00:25:00 +0900

    [Bronze V] Title: 검증수, Time: 124 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.java
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -3,23 +3,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
         StringTokenizer st = new StringTokenizer(str, " ");
-
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
-        int d = Integer.parseInt(st.nextToken());
-        int e = Integer.parseInt(st.nextToken());
-        int sixth = 0;
-
-        sixth = (int)(((Math.pow(a, 2)) + (Math.pow(b, 2)) + (Math.pow(c, 2))
-                + (Math.pow(d, 2)) + (Math.pow(e, 2)))% 10);
-
-        System.out.println(sixth);
+        
+        int sum = 0;
+        for(int i = 0; i < 5; i++){
+            int temp = Integer.parseInt(st.nextToken());
+            sum += temp * temp;
+        }
+        int result = sum % 10;
+        System.out.println(result);
         
     }
 }
